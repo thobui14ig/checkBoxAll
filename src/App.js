bc4eff81fdeb3c0c2ac292bee876bd95cbd34195
thobui1996 <buithanhtho20ig@gmail.com>
2022-08-10 00:49:57 +0700

    check box all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import data from './data'
 function App() {
+  const [totalPage, setTotalPage] = useState(null)
+  const [listData, setListData] = useState([])
+  const [active, setActive] = useState(1);
+  useEffect(() => {
+    //get tổng số trang
+    setTotalPage(Math.ceil(data.length / 3)); // mỗi trang hiển thị 3 phần tử
+    //lấy 3 phần tử đầu tiên
+    renderData(getListData(1))
+  }, []);
+
+  const getListData = (value) => {
+    let vitri = (value * 3) - 3;
+
+    const arr = [];
+    data.map((item, index) => {
+      if(index >= vitri && index <= vitri + 2) {
+        arr.push(item)
+      }
+    })
+    return arr;
+  }
+
+  const handleChonTrang = (position) => {
+    renderData(getListData(position))
+    setActive(position)
+  }
+  
+  const renderData = (listData, check = false) => {
+    const newArr = listData.map(item => {
+      return { ...item, check}
+    })
+    console.log(newArr);
+    setListData(newArr)
+  }
+
+  const handleCheckAll = (event) => {
+    if (event.target.checked) {
+      renderData(listData, true)
+    } else {
+      renderData(listData)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Check all <input type={'checkbox'} onClick={(e) => handleCheckAll(e)}/></p>
+      <table>
+        <thead>
+          <tr>
+            <th>Check</th>
+            <th>Company</th>
+          </tr>          
+        </thead>
+        <tbody>
+          {listData.length > 0 &&
+              listData.map((item, index) => {
+                return(
+                  <tr key={index}>
+                    <td><input checked={item.check ? 'checked' : ''} type={'checkbox'}/></td>
+                    <td>{item.name}</td>
+                  </tr>                  
+                )
+              })
+          }
+        </tbody>
+
+      </table>  
+      <div className="pagination">
+        <a href="#">&laquo;</a>
+        {[...Array(totalPage)].map((x, i) =>
+          <a className={i + 1 === active ? 'active' : ''} onClick={() => handleChonTrang(i + 1)} key={i}>{i + 1}</a>
+        )}
+        <a href="#">&raquo;</a>
+      </div>  
+    </>
+
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,14 @@
+const data = [
+    { id: 1, name: 'Data 1' },
+    { id: 2, name: 'Data 2' },
+    { id: 3, name: 'Data 3' },
+    { id: 4, name: 'Data 4' },
+    { id: 5, name: 'Data 5' },
+    { id: 6, name: 'Data 6' },
+    { id: 7, name: 'Data 7' },
+    { id: 8, name: 'Data 8' },
+    { id: 9, name: 'Data 9' },
+    { id: 10, name: 'Data 10' },
+]
+
+export default data;
